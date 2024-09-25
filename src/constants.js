ec8d002ef4667b791d989514e6444ae843891545
JAVED AKHTAR <javedsahil2002@gmail.com>
2024-09-25 14:59:21 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@
 export const name = 'Cehpoint E-Learning AI Solutions';
  export const company = 'Cehpoint E-Learning & Cybersecurity AI Solutions';
 export const websiteURL = 'https://ai-based-training-platfo-ca895.web.app';
-export const serverURL = 'http://localhost:5002/';
+export const serverURL = 'http://localhost:5002/';//run backend locally and put the link here
 export const logo = 'https://firebasestorage.googleapis.com/v0/b/ai-based-training-platfo-ca895.appspot.com/o/ai-technology.png?alt=media&token=6d27fd25-93e6-4b0e-b39e-3228e82668b6';
 
 // export const razorpayEnabled = true;
